8d9e34c38651c6b528bb86a2b1dffae62f6bc45a
xutongle <xutongle@gmail.com>
2018-02-12 14:23:21 +0800

    test

--- web/index.php
+++ web/index.php
@@ -1,15 +1,11 @@
 <?php
-if(getenv('APP_ENV') == 'Development'){
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
-} else {
-    defined('YII_DEBUG') or define('YII_DEBUG', false);
-    defined('YII_ENV') or define('YII_ENV', 'prod');
-}
-
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yuncms/cms/Yun.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../vendor/yuncms/cms/config/main.php'),
+    require(__DIR__ . '/../vendor/yuncms/cms/config/web.php'),
+    require(__DIR__ . '/../config/web.php')
+);
 
 (new yuncms\web\Application($config))->run();
