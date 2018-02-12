908f54680675295f07adf538ce6bd526264a0f7d
xutongle <xutongle@gmail.com>
2018-02-12 14:19:38 +0800

    update new

--- web/index.php
+++ web/index.php
@@ -1,12 +1,15 @@
 <?php
-
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+if(getenv('APP_ENV') == 'Development'){
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+} else {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../vendor/yuncms/cms/Yun.php');
 
 $config = require(__DIR__ . '/../config/web.php');
 
-(new yii\web\Application($config))->run();
+(new yuncms\web\Application($config))->run();
